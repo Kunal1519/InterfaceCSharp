4e658a10b2f7cba227dcf430982686fd85aead8d
HP <HP@LAPTOP-4U0HB9FI>
2022-08-26 11:36:08 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Interface
+{
+    public class Program:IMethod
+    {
+        public static void Main()
+        {
+            Program program = new Program();
+            program.Calculate(5, 29);
+        }
+
+        public void Calculate(int a, int b)
+        {
+           int sum = 0;
+            sum = a + b;
+            Console.WriteLine(sum);
+        }
+    }
+}
